e75bc6d05f81259753ac54718ccd6ec06f0fde30
Argho Das <aiub2016@yahoo.com>
2021-11-30 00:04:48 +0600

    Add files via upload

---
+++ Data Access Layer/Database.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Database
+    {
+       public Infos InfoTable { get; set; }
+
+        public Database()
+        {
+            InfoTable = new Infos();
+        }
+    }
+}
