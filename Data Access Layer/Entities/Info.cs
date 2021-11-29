20b0cad51de465e39aec6b23fb7139aa40c5cd0a
Argho Das <aiub2016@yahoo.com>
2021-11-30 00:06:11 +0600

    Add files via upload

---
+++ Data Access Layer/Entities/Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Info
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
